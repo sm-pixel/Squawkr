b4b4f3bcea67a7ec8b9a31c94d748ad421b83283
ryand67 <rwdevenney@gmail.com>
2020-06-15 14:08:41 -0400

    Routes to pages

--- controllers/hb-routes.js
+++ controllers/hb-routes.js
@@ -1,7 +1,15 @@
 const db = require('../models');
 
 module.exports = function(app){
-    app.get('/', (req, res) => {
-        res.render('index')
+    app.get('/' || '/login', (req, res) => {
+        res.render('login')
+    })
+
+    app.get('/signup', (req, res) => {
+        res.render('signup');
+    })
+
+    app.get('/home', (req, res) => {
+        res.render('home');
     })
 }

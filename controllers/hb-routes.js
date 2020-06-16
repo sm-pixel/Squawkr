4e26ac1785abdee9c5aeeaa6e3099ce2b51f8257
Khamari Thompson <61212885+Khamari13@users.noreply.github.com>
2020-06-16 17:13:41 -0400

    updated hb-routes.js and login.js files

--- controllers/hb-routes.js
+++ controllers/hb-routes.js
@@ -1,9 +1,14 @@
 const db = require('../models');
 
 module.exports = function(app){
-    app.get('/' || '/login', (req, res) => {
+    app.get('/', (req, res) => {
         res.render('login')
     })
+
+    app.get('/login', (req, res) => {
+        res.render('login')
+    })
+
 
     app.get('/signup', (req, res) => {
         res.render('signup');

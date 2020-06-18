a0da1ef14ca85becafd9c0ae363fc79934e2a392
ryand67 <rwdevenney@gmail.com>
2020-06-18 11:57:00 -0400

    Values auto fill

--- public/js/update.js
+++ public/js/update.js
@@ -2,10 +2,10 @@ $(document).ready( function () {
     console.log("connected")
     $.get("/api/user_data").then(function(data) {
         console.log(data)
-        $("#bio").text(data.bio);
-        $("#name").text(data.name);
-        $("#location").text(" - " + data.location);
-        $("#profilePicture").attr('src', data.profilePic)
+        $("#bio").val(data.bio);
+        $("#name").val(data.name);
+        $("#location").val(data.location);
+        $("#profilePicLink").val(data.profilePic)
       });
 
     $("#signuperror").hide()
